7dbb92e600ba82f6f9ecaf8e549b9e65dc8a119c
doit-now <hoang.nguyenthe@gmail.com>
2024-06-11 10:01:27 +0700

    update main code to use recursion

--- src/main/java/com/giaolang/mathutil/core/MathUtility.java
+++ src/main/java/com/giaolang/mathutil/core/MathUtility.java
@@ -40,12 +40,22 @@ public class MathUtility {
         if (n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be between 0..20");
         
-        if (n == 0)
+        if (n == 0 || n == 1)
             return 1;
         
-        for (int i = 1; i <= n; i++) 
-            product *= i; //thuật toán nhân dồn, con heo đất
-                          //ốc bu nhồi thịt
-        return product;
+        return n * getFactorial(n - 1);
+        //recursion - đệ quy - gọi lại chính mình với 1 quy mô khác
     }
 }
+
+//5! = 1.2.3.4.5 
+//   = 5 . 4!
+//4! = 1.2.3.4 
+//   = 4 . 3!
+//3! = 3 x 2!
+//2! = 2 x 1!
+//1! = 1
+//n! = n x (n - 1)! công thức dệ quy - recursion
+
+
+//búp bê người Nga
